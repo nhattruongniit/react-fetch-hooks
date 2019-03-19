6d2d358f7c374d0aebbcc3ff4d4456cf273d617a
Truong <truong.nguyen@persol.co.jp>
2019-03-19 16:32:53 +0700

    fetching data with hooks

--- src/App.js
+++ src/App.js
@@ -1,28 +1,71 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { Fragment, useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+const App = () => {
+  const [data, setData] = useState({ hits: [] });
+  const [query, setQuery] = useState('redux');
+  const [url, setUrl] = useState('http://hn.algolia.com/api/v1/search?query=redux');
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setError] = useState('');
+
+  const fetchData = async () => {
+    setIsLoading(true);
+    try {
+      const result = await axios(url);
+      document.title = 'Fetched data';
+      setData(result.data);
+    } catch (error) {
+      setError(error);
+    }
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    console.log('useEffect'); 
+    document.title = 'Fetching';
+    fetchData();
+  }, [url]);
+  
+
+  const handleFetch = (e) => {
+    e.preventDefault();
+    setUrl(`http://hn.algolia.com/api/v1/search?query=${query}`)
   }
+
+  return (
+    <Fragment>
+     <form
+        onSubmit={handleFetch}
+      >
+        <input
+          type="text"
+          value={query}
+          onChange={event => setQuery(event.target.value)}
+        />
+        <button type="submit">Search</button>
+      </form>
+
+      { isError && <div>Something went wrong ...</div> }
+      
+      { isLoading ? (
+        <div>Loading ...</div>
+      ) : (
+          <ul>
+          { 
+            data.hits.map(item => (
+              <li key={item.objectID}>
+                <span>{item.title}</span>
+              </li>
+            ))
+          }
+          </ul>
+        )
+      }
+     
+    </Fragment>
+    
+  )
 }
 
 export default App;
